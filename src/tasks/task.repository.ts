249e082a83361eb599ce5be2657c94126e0137fa
julysNICK <julystube@gmail.com>
2023-07-05 18:46:06 -0300

    insert new builder

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -2,6 +2,7 @@ import { Repository } from 'typeorm';
 import { Task } from './dto/task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskStatus } from './dto/task-status.enum';
+import { GetTasksFilterDto } from './dto/get-task-filter.dto';
 
 // export class TaskRepository extends Repository<Task> {}
 
@@ -27,5 +28,23 @@ export class TaskRepository extends Repository<Task> {
     task.status = status;
     await this.save(task);
     return task;
+  }
+
+  async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = filterDto;
+    const query = this.createQueryBuilder('task');
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+    if (search) {
+      query.andWhere(
+        '(task.title LIKE :search OR task.description LIKE :search)',
+        {
+          search: `%${search}%`,
+        },
+      );
+    }
+    const tasks = await query.getMany();
+    return tasks;
   }
 }
