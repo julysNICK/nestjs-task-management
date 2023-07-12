11a4a1691c2cd1b85de546a07d114425a8b72c12
julysNICK <julystube@gmail.com>
2023-07-12 15:54:28 -0300

    add envs configs

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -21,11 +21,15 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetUser } from 'src/auth/get-user.decorator';
 import { User } from 'src/auth/user.entity';
 import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 @Controller('tasks')
 @UseGuards(AuthGuard())
 export class TasksController {
   private logger = new Logger('TasksController');
-  constructor(private tasksService: TasksService) {}
+  constructor(
+    private tasksService: TasksService,
+    private configService: ConfigService,
+  ) {}
 
   @Get('/')
   getTasks(
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -4,10 +4,15 @@ import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TaskRepository } from './task.repository';
 import { AuthModule } from 'src/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([TaskRepository]),
+    AuthModule,
+  ],
   controllers: [TasksController],
   providers: [TasksService],
-  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule],
 })
 export class TasksModule {}
